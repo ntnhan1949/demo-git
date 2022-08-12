1916895e14b4b218b9c94f12a964e11ad3842915
Nguyen Thanh Nhan <ntnhan190499@gmail.com>
2022-08-12 10:36:19 +0700

    Create New.css file, update New.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import New from './component/New';
 import New2 from './component/New2';
 
 function App() {
   return (
     <div className="App">
-     <New2 />
+    <New />
+    <New2 />
     </div>
   );
 }
