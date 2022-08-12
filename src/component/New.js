eaa1454ce361b46db47b93c0f3911a2f653df494
Nguyen Thanh Nhan <ntnhan190499@gmail.com>
2022-08-12 17:22:43 +0700

    Update branch

--- src/component/New.js
+++ src/component/New.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
 import './New.css';
 import New2 from './New2';
+import Form from './form/Form';
 
 export default function New() {
 
@@ -15,14 +17,18 @@ export default function New() {
     return (
         <div className="container-fluid">
             <div className="header">
-                <a onClick={() => changeValue("Demo_Git", 0)} href="#default" className="logo">Demo_Git</a>
+                <Link onClick={() => changeValue("Demo_Git", 0)} to="#default" className="logo">Demo_Git</Link>
                 <div className="header-right">
-                    <a onClick={() => changeValue("Home", 0)} className={active === 0  ? "active" : ""} href="#home">Home</a>
-                    <a onClick={() => changeValue("Contact", 1)} className={active === 1  ? "active" : ""} href="#contact">Contact</a>
-                    <a onClick={() => changeValue("Login", 2)} className={active === 2  ? "active" : ""} href="./form.js">Login</a>
+                    <Link onClick={() => changeValue("Home", 0)} className={active === 0  ? "active" : ""} to="/">Home</Link>
+                    <Link onClick={() => changeValue("Contact", 1)} className={active === 1  ? "active" : ""} to="#contact">Contact</Link>
+                    <Link onClick={() => changeValue("Login", 2)} className={active === 2  ? "active" : ""} to="/login">Login</Link>
                 </div>
             </div>
             <New2 text={text} />
+            <Routes>
+                <Route path='/' element={<New2 />} />
+                <Route path='/login' element={<Form />} />
+            </Routes>
         </div>
     )
 }
--- src/component/form/form.js
+++ src/component/form/form.js
@@ -6,11 +6,6 @@ export default function Form() {
         <section className="ftco-section">
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-md-6 text-center mb-5">
-                        <h2 className="heading-section">Login #07</h2>
-                    </div>
-                </div>
-                <div className="row justify-content-center">
                     <div className="col-md-12 col-lg-10">
                         <div className="wrap d-md-flex">
                             <div className="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
