5bcd78cbd00e5cda1463d06d0af64f6c41940752
Nguyen Thanh Nhan <ntnhan190499@gmail.com>
2022-08-19 13:50:37 +0700

    Update branch main

--- src/index.js
+++ src/index.js
@@ -8,13 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
-      <App />
-=======
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
->>>>>>> branch1
+      <App />       
   </React.StrictMode>
 );
 
