cecdad70449e4d6563259fe81268aba8742fdb3f
Nguyen Thanh Nhan <ntnhan190499@gmail.com>
2022-08-19 14:37:37 +0700

    Create branch1.js

---
+++ src/component/Branch1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Branch1() {
+  return (
+    <div className='text'>This is branch 1</div>
+  )
+}
--- src/component/New.js
+++ src/component/New.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Branch1 from './Branch1';
 import './New.css';
 import New2 from './New2';
 
@@ -21,11 +22,11 @@ export default function New() {
                     <a onClick={() => changeValue("Home", 0)} className={active === 0 ? "active" : ""} href="#home">Home</a>
                     <a onClick={() => changeValue("Contact", 1)} className={active === 1 ? "active" : ""} href="#contact">Contact</a>
                     <a onClick={() => changeValue("About", 2)} className={active === 2 ? "active" : ""} href="#about">About</a>
-
                     <a onClick={() => changeValue("Branch", 3)} className={active === 3 ? "active" : ""} href="#branch">Branch</a>
                 </div>
             </div >
             <New2 text={text} />
+            <Branch1 />
         </div>
     )
 }
