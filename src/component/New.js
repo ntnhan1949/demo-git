e586c350e820afc48fb8d4bdf97bdc8721bd4d8a
Nguyen Thanh Nhan <ntnhan190499@gmail.com>
2022-08-19 13:21:16 +0700

    Update New.js

--- src/component/New.js
+++ src/component/New.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom';
 import './New.css';
 import New2 from './New2';
-import Form from './form/Form';
 
 export default function New() {
 
@@ -20,8 +19,8 @@ export default function New() {
                 <Link onClick={() => changeValue("Demo_Git", 0)} to="#default" className="logo">Demo_Git</Link>
                 <div className="header-right">
                     <Link onClick={() => changeValue("Home", 0)} className={active === 0  ? "active" : ""} to="/">Home</Link>
-                    <Link onClick={() => changeValue("Contact", 1)} className={active === 1  ? "active" : ""} to="#contact">Contact</Link>
-                    <Link onClick={() => changeValue("Branch", 2)} className={active === 2  ? "active" : ""} to="/login">Branch</Link>
+                    <Link onClick={() => changeValue("Contact", 1)} className={active === 1  ? "active" : ""} to="#">Contact</Link>
+                    <Link onClick={() => changeValue("Branch", 2)} className={active === 2  ? "active" : ""} to="#">Branch</Link>
                 </div>
             </div>
             <New2 text={text} />
